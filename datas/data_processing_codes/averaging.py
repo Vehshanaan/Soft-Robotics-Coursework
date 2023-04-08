5e5852a7e1b00db3595f8af4b6581fd8bc781fc6
“Vehshanaan” <1959180242@qq.com>
2023-04-08 17:56:38 +0100

    位移计量示意图

--- datas/data_processing_codes/averaging.py
+++ datas/data_processing_codes/averaging.py
@@ -2,7 +2,7 @@
 Author: “Vehshanaan” 1959180242@qq.com
 Date: 2023-04-08 12:51:08
 LastEditors: “Vehshanaan” 1959180242@qq.com
-LastEditTime: 2023-04-08 13:15:05
+LastEditTime: 2023-04-08 17:35:06
 FilePath: \Soft-Robotics-Coursework\datas\data_processing_codes\averaging.py
 Description: 
 
@@ -10,34 +10,45 @@ Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
 '''
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 folder_path = r"A:\OneDrive\MScRobotics\SR (Soft Robotics)\Soft-Robotics-Coursework\datas\90s\\"
 
 offset_path = ["1.npy", "2.npy", "3.npy", "4.npy", "5.npy"]
 
-data = []
+datas = []
 
 
 for i in range(len(offset_path)):
-    data.append(np.load(folder_path+offset_path[i]))
+    datas.append(np.load(folder_path+offset_path[i]))
 
 
 result = []
 
-
-# 对于每一个底部形变值：
 for current_bottom in range(0, 400):
+
     top_sum = 0
     top_count = 0
-    # 对于每一个数据：
-    for i in range(len(offset_path)):
-        # 分裂其顶部和底部
-        top = data[i][:,0]
-        bottom = data[i][:,1]
-        # 根据当前找的底部形变值找到对应的顶部形变值们的索引
-        # 记录索引的数量
-        # 将顶部形变值加到外层sum中，也记得加计数
-    if top_count == 0:continue
+
+    for data in datas:
+        top = data[:, 0]
+        bottom = data[:, 1]
+        for label in range(len(bottom)):
+            if bottom[label] == current_bottom:
+                top_sum += top[label]
+                top_count += 1
+
+    if top_count == 0:
+        continue
 
     top_average = top_sum/top_count
-    result.append([top_average,bottom])
 
+    result.append((current_bottom, top_average))
+
+result = np.array(result)
+
+x = result[:, 0]
+y = result[:, 1]
+
+plt.plot(x[::10],y[::10])
+plt.show()
--- datas/data_processing_codes/drawing.py
+++ datas/data_processing_codes/drawing.py
@@ -2,7 +2,7 @@
 Author: “Vehshanaan” 1959180242@qq.com
 Date: 2023-04-05 16:26:30
 LastEditors: “Vehshanaan” 1959180242@qq.com
-LastEditTime: 2023-04-08 13:34:21
+LastEditTime: 2023-04-08 17:32:44
 FilePath: \Soft-Robotics-Coursework\datas\data_processing_codes\drawing.py
 Description: 
 
@@ -19,30 +19,29 @@ raw = np.load(path)
 
 
 # 原始数据: 若干个二维点。
-#[a][b]
-#[a]是第几个点
+# [a][b]
+# [a]是第几个点
 # [b] 0 = 上面坐标，1=下面坐标
 
 # 提取上面点
-top = raw[:,0]
+top = raw[:, 0]
 
 # 提取下面点
-bottom = raw[:,1]
+bottom = raw[:, 1]
 
-#bottom=bottom[::10]
-#top=top[::10]
+# bottom=bottom[::10]
+# top=top[::10]
 
 
-
-plt.plot(bottom,top)
+plt.plot(bottom, top)
 
 plt.xlabel("bottom")
 plt.ylabel("top")
 
-plt.xlim(-1,300)
-plt.ylim(-1,300)
+plt.xlim(-1, 300)
+plt.ylim(-1, 300)
 
-#plt.gca().set_aspect("equal",adjustable="box")
+# plt.gca().set_aspect("equal",adjustable="box")
 
 plt.legend()
 
